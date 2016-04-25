0939851aca10a03410ec4a9cc5046948def42595
Naruto <zhiyou3g@lu.local>
2016-04-25 22:01:36 +0800

    Initial Commit

---
+++ 789/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  789
+//
+//  Created by Naruto on 16/4/25.
+//  Copyright © 2016年 Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 789/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  789
+//
+//  Created by Naruto on 16/4/25.
+//  Copyright © 2016年 Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
